c144ce21e38e6d5ea7747603c7b3c57df7852eba
mehakmehak <mehak1@sheridancollege.ca>
2019-05-22 09:37:43 -0400

    Pull commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author Mehak
+ */
+public class Student {
+    private int ID;
+    private String firstName;
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author Mehak
+ */
+public class StudentWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setFirstName("John");
+        s1.setID(4);        // TODO code application logic here
+    }
+    
+}
